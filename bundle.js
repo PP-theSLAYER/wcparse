6034859a17966eb6be914dedaa972fd88024dc7a
PP-theSLAYER <pp-theslayer@outlook.com>
2022-08-28 13:45:29 +1000

    Testing

--- bundle.js
+++ bundle.js
@@ -37,6 +37,8 @@ exports.parseWCBuffer = (buf, options) => {
   
 	if (buf.length == 1446) { // BOSS
 		return parseWCFullData(buf.slice(662), options);
+  } else if (buf.length == 720) { // WC8
+		return parseWC8Data(buf.slice(720), options);
   } else if (((buf.slice(662).length % 784 == 0)) & (buf.length > 1446)) { // multi-BOSS
 		return parseMultiWCFullData(buf.slice(662), options);
 	} else if (buf.length == 784) { // wcfull
